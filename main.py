98777c9e386a970f80a07a2ba5db016d1f44b10b
alshansky-g <rgcs.rgcs@gmail.com>
2025-09-10 02:56:21 +0300

    feat: add some basic structure modules

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import uvicorn
 from fastapi import FastAPI
+from items_views import router as items_router
+from users.views import router as users_router
 
-print("hello")
+app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
+
+
+@app.get("/")
+def get_index():
+    return {"message": "Hello from FastAPI"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
